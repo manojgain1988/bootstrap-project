6cced981d88c1f83a5bc824576928f624dda8362
manojgain1988 <manojgain1988@gmail.com>
2023-11-12 15:50:26 +0600

    TopScroll Add

--- js/apps.js
+++ js/apps.js
@@ -1,1 +1,15 @@
-alert('ok');
+
+function topScroll(){
+
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
+
+  
+// function  topScroll(){
+//     window.scrollTo({
+//         top:0,
+//       right:0,
+//       behavior:"smooth"
+//     })
+// }
